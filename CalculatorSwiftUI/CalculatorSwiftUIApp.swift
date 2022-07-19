e2982a65caf150018f3a64f85e424471e86882d9
YoungWoon Lee <lywzz6342@gmail.com>
2022-07-19 09:50:28 +0900

    Project Create

---
+++ CalculatorSwiftUI/CalculatorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorSwiftUIApp.swift
+//  CalculatorSwiftUI
+//
+//  Created by YoungWoon Lee on 2022/07/19.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
